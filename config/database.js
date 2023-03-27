4ee1a68243fc4570a645a95d043b43bc62f27297
ManuAT <atmanu7@gmail.com>
2023-03-27 10:08:02 +0000

    fix

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
-    remoteUrl : 'mongodb+srv://manu:cat007@cluster0-axhr4.mongodb.net/manu',
+    remoteUrl : 'mongodb+srv://manu:Y8B4MruGKzc93yPm@cluster0-axhr4.mongodb.net/manu',
     localUrl: 'mongodb://localhost/manu'
 };
